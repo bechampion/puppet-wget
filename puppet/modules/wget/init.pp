3bfcde58ad29e24f7719b931e107310b4ca0eef4
Jeronimo Garcia <jerry@spantree.net>
2014-03-20 09:43:20 -0300

    puppet-wget

---
+++ puppet/modules/wget/init.pp
@@ -0,0 +1,37 @@
+class wget {
+
+
+define download (
+                  $app,
+                  $url,
+                  $destination
+                )
+
+{
+notify{"Attempting to download ${app} from ${url} to ${destination}":}
+
+exec { 
+      "$app" :
+          command => "wget $url -P $destination",
+          path => "/usr/local/bin:/bin:/usr/bin",
+          creates => ["${destination}/${app}.tar.gz","${destination}/${app}.tgz"];
+    
+      "$app-untar" :
+         command=>"tar xvfz ${destination}/${app}.t* -C ${destination}/",
+         path => "/usr/local/bin:/bin:/usr/bin",
+         creates => "${destination}/${app}/",
+         #require => [Exec["$app"]];
+}
+}
+}
+class run ( $command ,  $path ) {
+    exec { "runit" :
+      command => "${path}/${command}",
+      returns => [0,1]
+    } 
+}
+class setevn($envvar,$value) {
+    file { "/etc/environment":
+        content => inline_template("${envvar}=${value}")
+    }
+}
